1231c78bbd752301f0a1d2183ed36a2754683780
King Liu <64719094+twmland@users.noreply.github.com>
2020-05-18 00:29:40 +0800

    跨執行緒存取控制項

--- ThreadShowOnController/Form1.Designer.cs
+++ ThreadShowOnController/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TxtBxNow = new System.Windows.Forms.TextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBxNow
+            // 
+            this.TxtBxNow.Location = new System.Drawing.Point(13, 13);
+            this.TxtBxNow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtBxNow.Name = "TxtBxNow";
+            this.TxtBxNow.ReadOnly = true;
+            this.TxtBxNow.Size = new System.Drawing.Size(148, 27);
+            this.TxtBxNow.TabIndex = 0;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(13, 56);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(148, 23);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 299);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.TxtBxNow);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TxtBxNow;
+        private System.Windows.Forms.Button BtnStart;
     }
 }
 
--- ThreadShowOnController/Form1.cs
+++ ThreadShowOnController/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,40 @@ namespace ThreadShowOnController
 {
     public partial class Form1 : Form
     {
+        private delegate void DisplayTime(TextBox txtBx);
+        private void ShowCurrentTime(TextBox txtBx)
+        {
+            txtBx.Text = DateTime.Now.ToString("hh:MM:ss");     
+        }
+       
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            ParameterizedThreadStart threadStart = new ParameterizedThreadStart(ShowTime);
+            Thread thread = new Thread(threadStart);
+            thread.Start(null);
+        }
+        private void ShowTime(object obj)
+        {
+            while (true)
+            {
+                Thread.Sleep(100);
+                if (TxtBxNow.InvokeRequired)
+                {
+                    DisplayTime displayTimeDelegate = new DisplayTime(ShowCurrentTime);
+                    this.Invoke(displayTimeDelegate, TxtBxNow);
+                }
+                else
+                {
+                    TxtBxNow.Text = DateTime.Now.ToString("hh:MM:ss");
+                }
+            }
+        }
+
+
     }
 }
